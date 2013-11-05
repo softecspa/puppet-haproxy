3d09d10131033487b92973af4be1b5361edbef6f
root on puppetmaster <root@puppetmaster.vagrant.local>
2013-11-05 11:43:15 +0100

    aggiunte define per balance generico, nrpe, ssh, smtp

--- manifests/nrpe_balance.pp
+++ manifests/nrpe_balance.pp
@@ -1,47 +1,30 @@
-define haproxy::ssh_balance (
+define haproxy::nrpe_balance (
   $local_ip,
   $bind_addresses,
   $backends,
   $backend_name   = '',
-  $nrpe_port       = '5666',
+  $nrpe_port      = '5666',
 ) {
 
-  $array_bind_addresses = is_array($bind_addresses)? {
-    true  => $bind_addresses,
-    false => [ $bind_addresses ],
+
+  haproxy::generic_tcp_balance { $name :
+    bind_addresses  => $bind_addresses,
+    backends        => $backends,
+    backend_name    => $backend_name,
+    port            => $nrpe_port,
   }
 
-  if !is_hash($backends) {
-    fail('parameter backends must be ah hash')
-  }
-
-  $string_binds = inline_template('<% array_bind_addresses.each do |bind| -%><%= bind %> <% end -%>')
-  if $string_binds !~ /([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\ )+$/ {
-    fail('invalid ip_address:port value present in bind_addresses')
-  }
-
-  $be_name = $backend_name? {
-    ''      => $name,
-    default => $backend_name
-  }
-
-  haproxy::backend {$be_name :}
-  create_resources(haproxy::backend::server,$backends, {'backend_name' => $be_name, 'port' => $nrpe_port})
-
-  haproxy::frontend {"frontend_${be_name}" :
-    bind            => $bind_addresses,
-    default_backend => $be_name,
-    port            => $nrpe_port
-  }
-
-  if !defined(Augeas['nrpe_local_bind']) {
-    augeas { 'nrpe_local_bind':
-      context => "/files/etc/nagios/nrpe.cfg",
-      changes => [
-        "set server_address $local_ip",
-      ],
-      notify  => Service['nagios-nrpe-server']
+  if !defined(File["${nrpe::nagiosconfdir}/local_bind.cfg"]) {
+    file {"${nrpe::nagiosconfdir}/local_bind.cfg":
+      ensure  => present,
+      content => "server_address=${local_ip}",
+      mode    => '644',
+      owner   => 'root',
+      group   => 'root',
+      notify  => Service['nagios-nrpe-server'],
+      before  => Service[$haproxy::params::service_name]
     }
   }
+
 
 }
--- manifests/ssh_balance.pp
+++ manifests/ssh_balance.pp
@@ -6,32 +6,11 @@ define haproxy::ssh_balance (
   $ssh_port       = '22',
 ) {
 
-  $array_bind_addresses = is_array($bind_addresses)? {
-    true  => $bind_addresses,
-    false => [ $bind_addresses ],
-  }
-
-  if !is_hash($backends) {
-    fail('parameter backends must be ah hash')
-  }
-
-  $string_binds = inline_template('<% array_bind_addresses.each do |bind| -%><%= bind %> <% end -%>')
-  if $string_binds !~ /([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\ )+$/ {
-    fail('invalid ip_address:port value present in bind_addresses')
-  }
-
-  $be_name = $backend_name? {
-    ''      => $name,
-    default => $backend_name
-  }
-
-  haproxy::backend {$be_name :}
-  create_resources(haproxy::backend::server,$backends, {'backend_name' => $be_name, 'port' => $ssh_port})
-
-  haproxy::frontend {"frontend_${be_name}" :
-    bind            => $bind_addresses,
-    default_backend => $be_name,
-    port            => $ssh_port
+  haproxy::generic_tcp_balance { $name :
+    bind_addresses  => $bind_addresses,
+    backends        => $backends,
+    backend_name    => $backend_name,
+    port            => $ssh_port,
   }
 
   if !defined(Augeas['ssh_local_bind']) {
